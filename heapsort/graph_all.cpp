5632b3079c877d7cf8724c61f358e9adaefcc3bc
gabrielfelipedy <gabriel.felipe.dy@gmail.com>
2024-03-03 13:57:42 -0300

    add heapsort implementations

--- heapsort/graph_all.cpp
+++ heapsort/graph_all.cpp
@@ -14,14 +14,14 @@ int main()
 	std::vector<double> times_c;
     std::vector<double> times_d;
     std::vector<double> times_r;
-	times_c = abreArquivo(times_c, "quick_sort_pm_c.txt");
-    times_d = abreArquivo(times_d, "quick_sort_pm_d.txt");
-    times_r = abreArquivo(times_r, "quick_sort_pm_r.txt");
+	times_c = abreArquivo(times_c, "heapsort_c.txt");
+    times_d = abreArquivo(times_d, "heapsort_d.txt");
+    times_r = abreArquivo(times_r, "heapsort_r.txt");
 
 	plt::named_plot("Crescente", times_c);
     plt::named_plot("Decrescente", times_d);
     plt::named_plot("Aleatório", times_r);
-    plt::title("Quick sort: Pivô como elemento central");
+    plt::title("HeapSort - Comparação de entradas");
     plt::legend();
     plt::xlabel("Número de elementos no array");
     plt::ylabel("Tempo de execução (ms)");
