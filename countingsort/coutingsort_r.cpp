dcd887225a7f5fa18a46eb89963c7a60feb08dec
gabrielfelipedy <gabriel.felipe.dy@gmail.com>
2024-03-05 10:22:10 -0300

    add counting sort folder

---
+++ countingsort/coutingsort_r.cpp
@@ -0,0 +1,37 @@
+#include <ctime>
+#include <cstdlib>
+#include "utils.h"
+#include "heapsort.h"
+
+#define QTDE 100000
+
+int main() {
+	srand(time(NULL));
+
+	std::vector<double> times = {}; //armazena os tempos de execução
+
+	int i = 1;
+	while(i <= QTDE)
+	{
+		int vet[i];
+
+		for(int j = 0; j < i; j++)
+			vet[j] = rand() % QTDE;
+
+		struct timeval start, end;
+
+		gettimeofday(&start, NULL);
+		heapsort(vet, i);
+		gettimeofday(&end, NULL);
+
+		double milliseconds = calcula_tempo(start, end);
+
+		times.push_back(milliseconds);
+
+		i++;
+	}
+	
+	escreveArquivo(times, "heapsort_r.txt");
+
+	return 0;
+}
